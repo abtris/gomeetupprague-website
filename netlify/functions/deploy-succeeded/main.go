ce06156f9a373d2d6db93da30d837e6107ac1f69
abtris <ladislav.prskavec@productboard.com>
2022-11-14 19:43:10 +0100

    Add netlify post-build function

---
+++ netlify/functions/deploy-succeeded/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type RequestBody struct {
+	Payload Payload `json:"payload"`
+}
+
+type Payload struct {
+	Context   string `json:"context"`
+	ReviewURL string `json:"review_url"`
+}
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+
+	requestBody := RequestBody{}
+	json.Unmarshal([]byte(request.Body), &requestBody)
+
+	if requestBody.Payload.Context == "deploy-preview" {
+		fmt.Println("previewURL: " + requestBody.Payload.ReviewURL)
+	} else {
+		fmt.Println("context " + requestBody.Payload.Context + " detected, skipping")
+	}
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode:      200,
+		Body:            "Success",
+		IsBase64Encoded: false,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
